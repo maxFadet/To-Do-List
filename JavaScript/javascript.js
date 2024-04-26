4327cfa9dd85d52475fb3e3ec853d854a9bbbc8e
Maksim Padrabinkin <averybodymax@gmail.com>
2024-04-26 16:16:20 +0200

    it was arranged

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -58,8 +58,8 @@
    ${task.done ? "style=\"text-decoration: line-through\"" : ""}
   >
   <button class="js-done">zrobione?</button>
-  <button class="js-remove">usuń</button>
   ${task.content}
+  <button class="js-remove">usuń</button>
   </li>
   `;
         };
@@ -78,7 +78,7 @@
 
         if (newTaskContent === "") {
             return;
-        }
+        };
 
         addNewTask(newTaskContent);
 
