e3e7954f4e2d32df67c18261419e98066ed8e34f
Maksim Padrabinkin <averybodymax@gmail.com>
2024-05-15 11:47:12 +0200

    changed the margins on narrow screens

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -94,12 +94,10 @@
         for (const task of tasks) {
             htmlString += `
         <li class="tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""}">
-          <button class="tasks__button tasks__button--toggleDone js-done"}"
-          >
-          ${task.done ? "✓" : ""}
+          <button class="tasks__button tasks__button--toggleDone js-done"}">
+            ${task.done ? "✓" : ""}
           </button>
-          <span class="tasks__content ${task.done ? "tasks__content--done" : ""}"
-          >
+          <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
             ${task.content}
           </span>
           <button class="tasks__button tasks__button--delete js-remove">
